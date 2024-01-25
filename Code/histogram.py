1d309507b77a79cd4777a43548e11c6a12dbe02d
DominicBarnett <dominic.barnett@icloud.com>
2024-01-24 20:56:25 -0800

    all words lowercase and finished the functions

--- Code/histogram.py
+++ Code/histogram.py
@@ -5,13 +5,19 @@ def read_words_file(file_path):
         words = file.read().splitlines()
     return words
 
+# LOOK AT MAKING ALL WORDS LOWERCASE AND TAKING OUT ALL PUNCTUATION
+# LOOK AT FROM COLLECTIONS IMPORT COUNTER (Don't do keep the written out code)
+
+
 one_line_txt = read_words_file(file_path)
 
 def string_txt_file(one_line_txt):
-    string_txt_file = ' '.join(one_line_txt)
+    base_string_txt_file = ' '.join(one_line_txt)
+    string_txt_file = base_string_txt_file.lower()
     return string_txt_file
 
 source_text = string_txt_file(one_line_txt).split()
+
 # print(source_text)
 
 
@@ -24,11 +30,15 @@ def histogram(source_text):
             histogram[i] = 1
     return histogram
 
-# word_count = histogram(source_text)
+word_count = histogram(source_text)
 # print(word_count)
 
 def unique_words(histogram):
-    pass
+    return len(histogram)
 
 def frequency(word, histogram):
-    pass
+    return histogram[word]
+
+word = input("What word would you like to know the frequency of?->  ")
+word_frequency = frequency(word, word_count)
+print(f'{word} appears {word_frequency} times')
